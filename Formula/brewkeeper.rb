717ff2bb94505c664b2e87647289fac2a43e9776
Patrick Jakobsen <patrick.jakobsen@icloud.com>
2026-03-23 17:42:26 +0100

    chore(formula): set v0.2.0 linux sha256

--- Formula/brewkeeper.rb
+++ Formula/brewkeeper.rb
@@ -11,7 +11,7 @@ class Brewkeeper < Formula
 
   on_linux do
     url "https://github.com/Pallepadehat/brewkeeper/releases/download/v0.2.0/brewkeeper-linux-x64.tar.gz"
-    sha256 "<REPLACE_LINUX_SHA256>"
+    sha256 "e663a1073bf023db12e05d1d671e288888726e6de82e74a51f0478cc2f243a4e"
   end
 
   def install
